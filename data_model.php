080803d04a0dfd42aa5d165ecc5999d4efa5c489
Leda909 <ko9zad@gmail.com>
2020-12-22 23:23:43 +0000

    Var3_pagi_from_database

--- data_model.php
+++ data_model.php
@@ -71,27 +71,65 @@ function get_product_by_id($product_id){
     }  
 }
 
-# Here we fetch from "Gallery" table.
-function get_all_gallery(){
+# Here we start pagination function to gallery page
+# declare variables to pagination function
+$current_page_number = '';
+$total_number_of_pages = '';
+$items_per_page = '';
+$start_pos ='';
 
-    global $conn;
-    $sql = "SELECT * FROM gallery";
+function pagination(){
+    global $conn, $current_page_number, $total_number_of_pages, $items_per_page, $start_pos ;
+    
+    # setting up the pagination
+    if ( isset( $_GET["current_page_number"] ) ) {
+        $current_page_number = $_GET["current_page_number"];
+    } else { 
+        $current_page_number = 1;
+    }
+
+    $items_per_page = 8;
+    $sql = "SELECT COUNT(*) AS count FROM gallery";
+    $result = mysqli_query($conn,$sql);
+
+    if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result)) {
+                    $total_items_in_db = $row["count"];
+                    $total_number_of_pages = ceil($total_items_in_db/ $items_per_page);
+                    $start_pos = ( $current_page_number * $items_per_page ) - $items_per_page;
+                }
+
+            } else {
+                echo "No product found";
+            }
+}
+
+
+# Here we fetch * from "Gallery" table to pagination
+function get_all_gallery(){
+    global $conn, $start_pos, $items_per_page ;
+
+    pagination();
+    $sql = "SELECT * FROM gallery LIMIT $start_pos, $items_per_page";
 
     $result = mysqli_query($conn, $sql);
 
-    $products = [];
+    $galleries = [];
+    // $galleries = array();
 
     if(mysqli_num_rows($result)> 0){
 
         while($row = mysqli_fetch_assoc($result)){
-                 array_push($products, $row);
+                 array_push($galleries, $row);
         }
-        return $products;
-
+        return $galleries;
     } else {
         return false;
     }
 }
+
+
+
 
 
 # -----------------------FETCH CART TABLE----------------------------#
--- gallery.php
+++ gallery.php
@@ -5,8 +5,26 @@
   $products = get_all_gallery();
 ?>
 
+<style>
+.pag {
+   text-align: center;
+   color: #6a4680;
+}
+.btn_pag {
+    background-color: #987fa6;
+    padding: 7px 14px 9px 14px;
+    color: white;
+    border-radius: 50%;
+}
+.btn_pag:hover {
+   color: white;
+   text-decoration: none;
+   background-color: #6a4680;
+}
+</style>
+
 <!-- Page Content -->
-<div class="bodybox container" style="margin-top: 17px; height: auto;" >
+<div class="container bodybox" style="margin-top: 20px; height: auto;" >
      
       <div class="portfolio-menu mt-2 mb-4">
          <ul>
@@ -19,17 +37,17 @@
       <div class="portfolio-item row">
          
          <?php 
-         foreach($products AS $product){ 
-            $product_id = $product['product_id'];
-            $product_category = $product['product_category'];
-            $product_type = $product['product_type'];
-            $product_image = $product['product_image'];
-            $product_title = $product['product_title'];   
+         foreach($galleries AS $tumbl){ 
+            $product_id = $tumbl['product_id'];
+            $product_category = $tumbl['product_category'];
+            $product_type = $tumbl['product_type'];
+            $product_image = $tumbl['product_image'];
+            $product_title = $tumbl['product_title'];   
          ?>
 
          <div class="item <?=$product_category ;?> col-lg-3 col-md-4 col-6 col-sm">
             <a href="images/<?=$product_image ;?>" class="fancylight popup-btn" data-fancybox-group="light"> 
-            <img class="img-fluid" src="images/<?=$product_image ;?>" alt=" <?=$product_title ;?>">
+            <img class="img-fluid" src="images/tumbl_<?=$product_image ;?>" alt=" <?=$product_title ;?>">
             <p style="background-color:#987fa6; color:white; font-size:20px; text-align:center;"><?=$product_title ;?></p> 
             </a>
          </div>  
@@ -37,6 +55,43 @@
 
       </div>
 
+      <div class="pag">
+         <!-- <a  href="?page=galleries&current_page_number=1">FIRST</a>  -->
+         <a class="btn_pag" href="?page=galleries&current_page_number=<?php echo ($current_page_number > 1 ? ($current_page_number - 1) : 1 )?>" style="margin-right:10px;">&laquo;</a> 
+         <span><?php echo $current_page_number; ?></span> / <span><?php echo $total_number_of_pages; ?> Page</span> 
+         <a class="btn_pag" href="?page=galleries&current_page_number=<?php echo ( ($current_page_number < $total_number_of_pages) ? ($current_page_number + 1) : $total_number_of_pages )?>" style="margin-left:10px;">&raquo;</a> 
+         <!-- <a href="?page=galleries&current_page_number=<?php echo $total_number_of_pages; ?>">LAST</a> -->
+      </div>
+
+      <div class="pag" style="margin-top: 15px;">
+         <a class="btn_pag" href="?page=products&current_page_number=1">&laquo;</a> 
+            <?php # Let's split our $total_number_of_pages using a for loop
+                  for($i=1; $i <= $total_number_of_pages; $i++){
+                     echo "&nbsp;<a class='btn_pag' href=?page=products&current_page_number=$i> $i  </a>&nbsp;";	  
+                  }?>
+         <a class="btn_pag" href="?page=products&current_page_number=<?php echo $total_number_of_pages; ?>">&raquo;</a>
+      </div>
+
+      <div class="pag" style="margin-top: 15px;">
+         <a  href="?page=products&current_page_number=1">&laquo;</a> 
+            <?php # Let's split our $total_number_of_pages using a for loop
+               for($i=1; $i <= $total_number_of_pages; $i++){
+                     # If $_GET is set with current_page_number and current_page_number == value of $i then we want to add "active" CSS class for that 
+                     if(isset($_GET['current_page_number']) && $_GET['current_page_number']==$i){
+                        $selected = "active";
+                     
+                        # If $_GET is NOT set with current_page_number and $i== 1 then we want to add "active" CSS for the (the first record)
+                        } elseif(!isset($_GET['current_page_number']) && $i==1){
+                           $selected = "active";
+                           
+                              } else {
+                                 $selected = "none";
+                              }
+                     echo "&nbsp;<a class=$selected href=?page=products&current_page_number=$i>$i</a>&nbsp;";	  
+               }?>
+         <a href="?page=products&current_page_number=<?php echo $total_number_of_pages; ?>">&raquo;</a>
+      </div>
+      
 </div>
 
 
