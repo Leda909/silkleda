0233e20ba606662f3b45629422643b01b50111ea
Leda909 <ko9zad@gmail.com>
2020-12-22 19:44:57 +0000

    add pagination.js

---
+++ footer.php
@@ -0,0 +1,45 @@
+
+<footer class="container-fluid bg-dark py-2" style="margin-top:35px; height:100px;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="row">
+
+        </div>
+      </div>   
+      <div class="col-lg-4">
+        <div class="row">
+        
+        </div>
+      </div>   
+      <div class="col-lg-4" style="height:85px;">
+        <div class="card-header text-white text-uppercase" style="font-weight:bold; border:none; position: absolute; display: inline-block;" >
+            <p style="position:relative; left:0; top:10px;">Follow me on:</p>
+            <a class="social btn-success btn-lg" style="left:120%;" href="https://www.facebook.com/Silk-Leda-112654040409129/" target="_blank">
+                <i class="fab fa-facebook"></i>
+            </a>
+            <a class="social btn-success btn-lg" style="left:130%;" href="https://www.instagram.com/leda_art_9/" target="_blank">
+                <i class="fab fa-instagram"></i>
+            </a>
+        </div>
+      </div>  
+    </div> 
+  </div>
+</footer>
+  
+
+
+  <!-- Bootstrap core JavaScript -->
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/isotope.pkgd.js"></script>
+  <script src="js/jquery.magnific-popup.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/custom_gallery.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
+  <script scr="js/pagination.js"></script>
+
+
+</body>
+
+</html>
