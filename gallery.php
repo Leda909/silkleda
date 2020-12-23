94a19c5b5d0c4ee6c53de8707727446a424862e4
Leda909 <ko9zad@gmail.com>
2020-12-23 17:50:25 +0000

    for loop moved to custom_gallery.js

--- gallery.php
+++ gallery.php
@@ -1,12 +1,10 @@
 <?php
-  include_once "header.php";
-  include_once "data_model.php";
-
-  $products = get_all_gallery();
+   include_once "header.php";
+   include_once "data_model_gallery.php";
 ?>
 
 <!-- Page Content -->
-<div class="bodybox container" style="margin-top: 17px; height: auto;" >
+<div class="container bodybox" style="margin-top: 20px; min-height: 1200px;" >
      
       <div class="portfolio-menu mt-2 mb-4">
          <ul>
@@ -14,27 +12,14 @@
             <li class="btn btn-outline-dark" data-filter=".Mandala">Mandala</li>
             <li class="btn btn-outline-dark" data-filter=".Picture">Painting</li>
          </ul>
+
+         <ul id="demo">
+         </ul>
       </div>
 
-      <div class="portfolio-item row">
-         
-         <?php 
-         foreach($products AS $product){ 
-            $product_id = $product['product_id'];
-            $product_category = $product['product_category'];
-            $product_type = $product['product_type'];
-            $product_image = $product['product_image'];
-            $product_title = $product['product_title'];   
-         ?>
-
-         <div class="item <?=$product_category ;?> col-lg-3 col-md-4 col-6 col-sm">
-            <a href="images/<?=$product_image ;?>" class="fancylight popup-btn" data-fancybox-group="light"> 
-            <img class="img-fluid" src="images/<?=$product_image ;?>" alt=" <?=$product_title ;?>">
-            <p style="background-color:#987fa6; color:white; font-size:20px; text-align:center;"><?=$product_title ;?></p> 
-            </a>
-         </div>  
-         <?php }?> 
-
+      <div class="portfolio-item">
+         <div class="data-container row">
+         </div>
       </div>
 
 </div>
