bb6ef8c2d4340354c74813877cf8abaf677104a9
Leda909 <ko9zad@gmail.com>
2020-12-23 17:36:30 +0000

    reorganize js + add pagination.js

--- footer.php
+++ footer.php
@@ -29,16 +29,16 @@
   
 
 
-  <!-- Bootstrap core JavaScript -->
+  <!-- External JavaScript Files -->
 
   <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
   <script src="js/isotope.pkgd.js"></script>
   <script src="js/jquery.magnific-popup.js"></script>
-  <script src="js/bootstrap.js"></script>
+  <script src="js/pagination.js"></script>
   <script src="js/custom_gallery.js"></script>
-  <script src="js/bootstrap.bundle.js"></script>
-
-
+  
 </body>
 
 </html>
