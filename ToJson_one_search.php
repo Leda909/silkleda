6b29322c88df843ab7dd61465f9ff655a2a9ea5c
Leda909 <ko9zad@gmail.com>
2020-12-23 18:04:56 +0000

    search item by keyword from gallery table

---
+++ ToJson_one_search.php
@@ -0,0 +1,30 @@
+<?php
+ob_start();
+include_once 'data_model_search.php';
+include_once 'data_model_gallery.php';
+
+header('Content-type:application/json;charset=utf-8');
+
+if(isset($_GET['keyword'])){
+      $keyword = $_GET['keyword'];
+      if (isset($keyword) && $keyword == '*'){
+        $search_gallery = get_all_gallery();
+      } elseif (isset($keyword) && $keyword !== ''){
+        $search_gallery = get_item_by_keyword($keyword);
+      } else {
+        echo json_encode(array("message" => "Please, give a keyword."));
+        return;
+      }
+  } else {
+    $search_gallery = get_all_gallery();
+}
+
+if($search_gallery){
+    $myJSON_G_I = json_encode($search_gallery);
+    echo $myJSON_G_I;
+    } else {
+        http_response_code(404);
+        // tell the user product does not exist
+        echo json_encode(array("message" => "Product does not exist."));
+    }
+?>
